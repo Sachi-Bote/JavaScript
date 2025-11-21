b7d53db2d546451f0a92a2106d7988c70a7859bd
Sachi Bote <sachibote@gmail.com>
2025-11-21 13:53:22 +0000

    commit 8

--- 03_objects/object2.js
+++ 03_objects/object2.js
@@ -1,1 +1,37 @@
 const IBUser = new Object()
+
+IBUser.id = "A12345"
+IBUser.name = "Hriday"
+IBUser.age = 20
+
+//console.log(IBUser);
+
+//Object in Object
+
+const INUser = {
+    email: "hriday@gmail.com",
+    fullname: {
+        userfullname: {
+        firstname: "Hriday",
+        lastname: "Dixit"
+        }
+    }
+}
+
+//console.log(INUser.fullname.userfullname.firstname);
+
+
+// COMBINE / CONCAT OBJECTS
+const obj1 ={
+    1: "a",
+    2: "b"
+}
+
+const obj2 ={
+    3: "c",
+    4: "d"
+}
+
+//const obj3 = {obj1, obj2}  //wrong method
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3);
