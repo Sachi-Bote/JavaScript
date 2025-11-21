c1a04e50e60ada0bb942f54687786da28be15deb
Sachi Bote <sachibote@gmail.com>
2025-11-21 14:52:32 +0000

    commit 9

---
+++ 04_functions/function.js
@@ -0,0 +1,41 @@
+//FUNCTION 1
+function sayMyName() {
+    console.log("S");
+    console.log("a");
+    console.log("c");
+    console.log("h");
+    console.log("i");
+}
+//sayMyName()
+
+
+//FUNCTION 2
+
+// function addTwoNumbers (num1, num2){
+//     console.log( num1 + num2);
+// }
+
+// function addTwoNumbers (num1, num2){
+//     // let result = num1 + num2
+//     // return result
+//     return (num1 + num2)
+// }
+// const result = addTwoNumbers(1, 2);
+// console.log(result);
+
+//FUNCTION 3
+
+// function loginUserMessage(username){
+//     return `${username} just logged in`
+// }
+
+function loginUserMessage(username = "xyz"){
+    if (!username){
+        console.log("Please enter a user name")
+        return
+    }
+    return `${username} just logged in`
+}
+//console.log(loginUserMessage("Hriday"));
+
+console.log(loginUserMessage());
